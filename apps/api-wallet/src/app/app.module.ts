29f227cb7056f5eccb8f1895dee573d66b534b08
Richard Betalmos <BETALMOS@MLHUILLIER.LOCAL>
2023-09-11 16:59:24 +0800

    configure app module for env

--- apps/api-wallet/src/app/app.module.ts
+++ apps/api-wallet/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.src.env',
+    }),
     ConfigModule.forFeature(DB_Config),
     WalletModule,
     OtpModule,
